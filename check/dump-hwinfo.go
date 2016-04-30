9ed2f80c3a8744f51c3d219f53d81b20bc8ed315
minhchuduc <chu.ducminh@gmail.com>
2016-04-30 11:09:12 +0700

    Add get Ram information

--- check/dump-hwinfo.go
+++ check/dump-hwinfo.go
@@ -8,4 +8,5 @@ import (
 func main() {
 	fmt.Println(Cpu)
 	fmt.Println(Motherboard)
+	fmt.Println(Ram)
 }
---
+++ raminfo.go
@@ -0,0 +1,70 @@
+package hwinfo
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+type Ramtype struct {
+	Size         string
+	BankLocator  string
+	ClockSpeed   string
+	Manufacturer string
+	SerialNumber string
+	PartNumber   string
+}
+
+type Ramstats struct {
+	TotalCapacity string
+	Items         []Ramtype
+}
+
+var Ram Ramstats
+
+func init() {
+	cmd := exec.Command("/usr/sbin/dmidecode", "-t", "memory")
+	buf, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	output := strings.Split(string(buf), "\n")
+
+	slot := -1
+
+Iterate_Line:
+	for _, line := range output {
+		line_arr := strings.Split(line, ":")
+		if len(line_arr) == 2 {
+			key, value := line_arr[0], line_arr[1]
+			if key == "\tMaximum Capacity" {
+				Ram.TotalCapacity = value
+			}
+			switch key {
+			case "\tArray Handle":
+				slot++
+				var dimm Ramtype
+				Ram.Items = append(Ram.Items, dimm)
+			case "\tSize":
+				Ram.Items[slot].Size = value
+			case "\tBank Locator":
+				Ram.Items[slot].BankLocator = value
+			case "\tSpeed":
+				Ram.Items[slot].ClockSpeed = value
+			case "\tManufacturer":
+				Ram.Items[slot].Manufacturer = value
+			case "\tSerial Number":
+				Ram.Items[slot].SerialNumber = value
+			case "\tPart Number":
+				Ram.Items[slot].PartNumber = value
+			}
+
+		} else {
+			continue Iterate_Line
+			/* Now it's just experimental "Label" in Go,
+			because i changed my mind & use a different algo. */
+		}
+
+	}
+}
